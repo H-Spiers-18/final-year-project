793887dd62b018f190b85db007e9c10414379165
h-spiers <harryspiers2000@gmail.com>
2022-02-10 13:03:43 +0000

    Added NF property boundary indexes to constants.py

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+# contains constants for my codebase. used to keep values consistent across the whole codebase
+
+# index for each dataset where the configuration options end and non-functional properties begin
+NODEJS_NF_BOUNDARY = -1
+POPPLER_NF_BOUNDARY = -2
+X264_NF_BOUNDARY = -5
+XZ_NF_BOUNDARY = -2
