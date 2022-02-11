8b7b8789d8ca1aa71f1becd6e0a52e16b1a316df
h-spiers <harryspiers2000@gmail.com>
2022-02-11 15:34:55 +0000

    Implemented fit and predict methods for PredictorLearner

--- src/learner.py
+++ src/learner.py
@@ -1,33 +1,41 @@
 from abc import ABC, abstractmethod
 
+from sklearn.tree import DecisionTreeRegressor
+
 
 class Learner(ABC):
 
     def __init__(self):
+        self.model = None
         super().__init__()
 
     @abstractmethod
-    def fit(self):
+    def fit(self, x_train, y_train):
         pass
 
     @abstractmethod
-    def predict(self):
+    def predict(self, x_test):
         pass
 
 
 class PredictorLearner(Learner):
 
-    def fit(self):
-        pass
+    def __init__(self):
+        super().__init__()
 
-    def predict(self):
-        pass
+    def fit(self, x_train, y_train):
+        self.model = DecisionTreeRegressor(random_state=20)
+        self.model.fit(x_train, y_train)
+
+    def predict(self, x_test):
+        y_test = self.model.predict(x_test)
+        return y_test
 
 
 class TransferLearner(Learner):
 
-    def fit(self):
+    def fit(self, x_train, y_train):
         pass
 
-    def predict(self):
+    def predict(self, x_test):
         pass
