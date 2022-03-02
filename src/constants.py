6a06b71f5ee8068da6157f42f673f8c571675750
h-spiers <harryspiers2000@gmail.com>
2022-03-02 11:16:50 +0000

    Added dataset csv filename constants to constants.py

--- src/constants.py
+++ src/constants.py
@@ -6,11 +6,17 @@ CV_FOLDS = 5
 # Experiment repetitions
 EXPERIMENT_REPS = 5
 
-# Dataset paths
+# Dataset folder paths
 NODEJS_PATH = '../dataset/nodejs'
 POPPLER_PATH = '../dataset/poppler'
 X264_PATH = '../dataset/x264'
 XZ_PATH = '../dataset/xz'
+
+# Dataset csv paths
+NODEJS_CSV_PATH = 'buffer1.csv'
+POPPLER_CSV_PATH = 'apache_camel.csv'
+X264_CSV_PATH = 'original_videos_Animation_480P_Animation_480P-087e.csv'
+XZ_CSV_PATH = 'dickens.csv'
 
 # Index for each dataset where the configuration options end and non-functional properties begin
 NODEJS_NF_BOUNDARY = -1
