74a48e0f79928ba45f6110c7e21c9bac66c9cb89
h-spiers <harryspiers2000@gmail.com>
2022-03-19 16:56:58 +0000

    Added cliffs_delta function to analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,3 +1,4 @@
+import numpy as np
 from scipy.stats import wilcoxon
 
 
@@ -16,3 +17,29 @@ def get_wilcoxon_p_value(s1, s2):
     """
     _, p = wilcoxon(s1, s2)
     return p
+
+
+def cliffs_delta(s1, s2):
+    """
+    Performs the Cliff's delta test to determine the effect size of my measurements. Based on the following link's
+    implementation, just written to efficiently use numpy arrays - https://github.com/txt/ase16/blob/master/doc/stats.md
+    Parameters
+    ----------
+    s1: numpy.ndarray - array of prediction values supporting the null hypothesis
+    s2: numpy.ndarray - array of prediction values supporting the alternative hypothesis
+    Returns
+    -------
+    z: float - z-value used to determine effect size.
+               z < 0.147 - negligible effect size
+               0.147 <= z < 0.33 - small effect size,
+               0.33 <= z < 0.474 - medium effect size,
+               0.474 <= z - large effect size
+               (see figure 11 in report, based on D. Chen et. al [31])
+    """
+    less_than_count = greater_than_count = 0
+    for x in s1:
+        greater_than_count += np.sum(s2 > x)
+        less_than_count += np.sum(s2 < x)
+
+    z = abs(less_than_count - greater_than_count) / (len(s1) * len(s2))
+    return z
