29159b3ff4b4d05b1014a00475a247de94e6bf42
h-spiers <harryspiers2000@gmail.com>
2022-04-04 10:58:00 +0100

    Added comment explaining each test

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -7,6 +7,7 @@ from src.analysis import *
 
 
 def test_output_structure():
+    """Check that the analysis output file structure is as expected"""
     assert sorted(os.listdir('../results/_analysis/rq1_analysis')) == ['mape_error_optimised_nodejs_box_plot.png',
                                                                        'mape_error_optimised_x264_box_plot.png',
                                                                        'mape_error_optimised_xz_box_plot.png',
@@ -33,6 +34,8 @@ def test_output_structure():
 
 
 def test_scatter_plot_output():
+    """Check that the scatter plot output file structure is as expected. Includes checking that plots for all subject
+    systems are present and that there is a plot for each experiment repetition"""
     scatter_plot_paths = constants.RQ_ANALYSIS_PATHS[:2]  # only check RQ1 and 2 since 3 has no scatter plots
     for rq_analysis_path in scatter_plot_paths:
         _path = os.path.join(rq_analysis_path, 'transfer_model_scatter_plots')
@@ -44,6 +47,8 @@ def test_scatter_plot_output():
 
 
 def test_mean_min_max_output():
+    """Check that the mean min max file output is as expected. Includes checking that the mean, min and max values are
+    calculated for each subject system"""
     for rq_analysis_path in constants.RQ_ANALYSIS_PATHS:
         mean_min_max_csv = pd.read_csv(os.path.join(rq_analysis_path, 'mean_min_max.csv'))
         assert len(mean_min_max_csv['measurement']) == 9
