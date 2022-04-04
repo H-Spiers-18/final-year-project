738773030bf0c3e9e791f294690d754e3ed9120d
h-spiers <harryspiers2000@gmail.com>
2022-04-04 11:06:54 +0100

    Made results column names a constant in constants.py

--- tests/test_rq_output.py
+++ tests/test_rq_output.py
@@ -12,25 +12,7 @@ def test_results_structure():
 
 def test_rq_csvs_contents():
     """Checks that the results csv contents are as they should be. Includes checking column names and column lengths"""
-    expected_columns = [
-        ['mse_accuracy_tgt_no_cv', 'mape_accuracy_tgt_no_cv', 'mse_accuracy_tgt_cv', 'mape_accuracy_tgt_cv',
-         'mse_accuracy_trans_no_cv', 'mape_accuracy_trans_no_cv', 'mse_accuracy_trans_cv', 'mape_accuracy_trans_cv'],
-        ['mape_accuracy_pred_20pct', 'mape_accuracy_pred_40pct', 'mape_accuracy_pred_60pct',
-         'mape_accuracy_pred_80pct', 'mape_accuracy_trans_20pct', 'mape_accuracy_trans_40pct',
-         'mape_accuracy_trans_60pct', 'mape_accuracy_trans_80pct', 'mse_accuracy_pred_20pct',
-         'mse_accuracy_pred_40pct', 'mse_accuracy_pred_60pct', 'mse_accuracy_pred_80pct',
-         'mse_accuracy_trans_20pct', 'mse_accuracy_trans_40pct', 'mse_accuracy_trans_60pct',
-         'mse_accuracy_trans_80pct'],
-        ['training_time_pred_20pct_no_cv', 'training_time_pred_40pct_no_cv',
-         'training_time_pred_60pct_no_cv',
-         'training_time_pred_80pct_no_cv', 'training_time_pred_20pct_cv', 'training_time_pred_40pct_cv',
-         'training_time_pred_60pct_cv', 'training_time_pred_80pct_cv',
-         'training_time_trans_20pct_no_cv',
-         'training_time_trans_40pct_no_cv', 'training_time_trans_60pct_no_cv',
-         'training_time_trans_80pct_no_cv',
-         'training_time_trans_20pct_cv', 'training_time_trans_40pct_cv', 'training_time_trans_60pct_cv',
-         'training_time_trans_80pct_cv']
-        ]
+    expected_columns = constants.RESULTS_DATAFRAME_COLUMN_NAMES
     # read all results CSVs
     results_csvs = [read_results_csv(os.path.join('../results/', subject_system, rq_csv))
                     for subject_system, rq_csv in zip(constants.SUBJECT_SYSTEMS, constants.RQ_CSV_NAMES)]
